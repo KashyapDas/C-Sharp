34b380f898ace7b7ab39a9ee9d73009a8832cc1f
Kashyap Das <kashyap.education2234@gmail.com>
2024-12-04 07:57:52 +0530

    creating the githb repo and for now pushing two C# practice problem in the github

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); // This method "WriteLine()" will create a new line 
+            Console.Write("Hello World"); // This method "Write()"will not create the new line.
+        }
+    }
+}
---
+++ UserInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kashyap
+{
+    class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name ? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"My name is {name}");
+        }
+    }
+}
