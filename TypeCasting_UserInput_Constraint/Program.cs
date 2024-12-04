46e959dc5a8cbd799e131fbdd007b26d17e7c38b
Kashyap Das <kashyap.education2234@gmail.com>
2024-12-04 09:22:29 +0530

    pushing C# code

---
+++ TypeCasting_UserInput_Constraint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Kashyap
+{
+    class TypeCasting_UserInput_Constraint
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name : ");
+            string ?name = Console.ReadLine();
+            Console.Write("What is you age : ");
+            int ?age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+        }
+    }
+}
