adc2e1dbf42a462288b9b596e35e201bdb6203f1
Kashyap Das <kashyap.education2234@gmail.com>
2024-12-07 21:26:52 +0530

    7th Dec 2024

---
+++ MathClass_C_Sharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kashyap
+{
+    class MathClass_C_Sharp
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the 1st number : ");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 2nd number : ");
+            int no2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The maximum between both the number is : {Math.Max(no1,no2)}");
+        }
+    }
+}
